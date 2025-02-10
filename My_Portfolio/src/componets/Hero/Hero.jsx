8d8f94274f559456938cbc777aec80df3a3a6e7d
Annpurna Sharma <annpurnasha474@gmail.com>
2025-02-10 19:33:06 +0530

    commit

--- My_Portfolio/src/componets/Hero/Hero.jsx
+++ My_Portfolio/src/componets/Hero/Hero.jsx
@@ -5,10 +5,10 @@ export const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi ,I'm Annpurna</h1>
-        <p className={styles.description}>I'm a full Stack using SQL & PHP and a MERN Stack developer.Reach out if you'd like to learn more!</p>
-   <a href="mailto:annpurnasha474@gmail.com"  className={styles.contectbtn}>Contect me</a>
+        <p className={styles.description}>I'm a full Stack Developer using SQL & PHP and a MERN Stack developer.Reach out if you'd like to learn more!</p>
+   <a href="mailto:annpurnasha474@gmail.com"  className={styles.contectbtn}>Contact me</a>
    </div>
-   <img src="../assest/19.37.25_e3b50aa8.jpg" alt="my img "  className={styles.img}/>
+   <img src="../assest/19.37.25_e3b50aa8.jpg" alt="my image "  className={styles.img}/>
    <div className={styles.topBlur} />
    <div className={styles.bottomBlur} />
    
